a83edd8816669e973c51dbed4d3b27f86018a58e
Kevin <kshermanstudios@gmail.com>
2023-07-26 15:08:19 -0700

    add form submit and close

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,42 @@
 import classes from './NewPost.module.css';
+import { useState } from 'react';
 
-function NewPost({changeTextArea, changeAuthor, onCancel }) {
+function NewPost({ onCancel }) {
  
+  const [enteredText, SetEnteredText] = useState('React is great');
+  const [enteredAuthor, SetAuthor] = useState('Kevin');
+  
+  const changeTextAreaHandler = (event) => {
+      SetEnteredText(event.target.value);
+  };
+
+  const changeAuthorHandler = (event) => {
+      SetAuthor(event.target.value);
+  };
+    
+  const submitHandler = (event) => {
+    event.preventDefault(); 
+
+    const postData ={
+      body: enteredText,
+      author: enteredAuthor
+    } 
+
+    console.log(postData);
+
+    // now we can close the modal window
+    onCancel();
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeTextArea}/>
+        <textarea id="body" required rows={3} onChange={changeTextAreaHandler}/>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={changeAuthor} />
+        <input type="text" id="name" required onChange={changeAuthorHandler} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>Cancel</button>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,34 +2,20 @@ import Post from './Post';
 import classes from './PostsList.module.css';
 import NewPost from './NewPost';
 import Modal from './Modal';
-import {useState} from 'react';
 
 function PostList({isPosting, onStopPosting}) {
-    const [enteredText, SetEnteredText] = useState('React is great');
-    const [enteredAuthor, SetAuthor] = useState('Kevin');
-    
-    const changeTextAreaHandler = (event) => {
-        SetEnteredText(event.target.value);
-    };
-
-    const changeAuthorHandler = (event) => {
-        SetAuthor(event.target.value);
-    };
     
     return (
     <>  
     {isPosting && (
         <Modal hideModal={onStopPosting} >
-            <NewPost 
-                changeTextArea={changeTextAreaHandler} 
-                changeAuthor={changeAuthorHandler}
+            <NewPost
                 onCancel={onStopPosting}
             />
         </Modal>
         )
     }
     <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredText} />
         <Post author='daisy' body="Check out the full lesson" />
     </ul>
     </>
